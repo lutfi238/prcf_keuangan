e95e274ec8faf13783f20bd8df1347448a1fbc97
Muhammad Lutfi Firdaus <lutfifirdaus238@gmail.com>
2025-11-03 22:11:20 +0700

    revert

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation'
-import { cookies } from 'next/headers'
-
-export default function Home() {
-  // This will be handled by middleware for authentication checks
-  // For now, redirect to login
-  redirect('/auth/login')
-}
